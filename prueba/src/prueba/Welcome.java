c7d9cfaecce5d3e33d77495c8550008f52ce0331
manuel <manuel@localhost>
2020-11-11 22:20:21 -0500

    Actualizado por primera vez.

---
+++ prueba/src/prueba/Welcome.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package prueba;
+
+/**
+ * @author manuel
+ *
+ */
+public class Welcome {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
